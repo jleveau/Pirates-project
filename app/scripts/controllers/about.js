78c05bfec8f99374bc4c97d065b8860ef2dbdaf8
Julien Leveau <oursgroumy@gmail.com>
2015-07-06 13:16:29 +0200

    creation du projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name piratesProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the piratesProjectApp
+ */
+angular.module('piratesProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
